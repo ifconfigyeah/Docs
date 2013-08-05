42d51118726759d4dec75735fe3f49f7399e6604
ifconfigyeah <ifconfig@yeah.net>
2013-08-05 11:19:37 +0800

    11
    11

--- myalram.py
+++ myalram.py
@@ -7,7 +7,7 @@ not_executed = 1
 version = '0.1 beta'
 playHour = 0
 playMin  = 0
-interval = 1              # interval time
+interval = 1              # 默认1小时的间隔时间
 
 def soundStart():
     if sys.platform[:5] == 'linux':
@@ -17,8 +17,9 @@ def soundStart():
         import winsound
         winsound.PlaySound(soundFile, winsound.SND_ASYNC)
         not_executed = 0
-        time.sleep(60)
-        winsound.PlaySound(soundFile, winsound.SND_PURGE)
+        time.sleep(60)                     # 播放闹铃一分钟后自动停止
+#        winsound.PlaySound(soundFile, winsound.SND_PURGE)
+        winsound.PlaySound("SystemExit", SND_PURGE)
 
 if __name__ == '__main__':
     if sys.platform == 'darwin':
@@ -28,11 +29,19 @@ if __name__ == '__main__':
         ct = list(time.localtime())
         curHour  = ct[3]
         curMin   = ct[4]
-        playHour = curHour + interval
+
+        playHour = curHour + interval 
+        if playHour == 24:
+            playhour = 0
+
         playMin  = curMin
+        if playMin == 60:
+            playHour = playHour + 1
+            playMin  = 0
+
+        print 'Alarm time is', playHour, ':', playMin
 
 while(not_executed):
-#    print 'Alarm time is', playHour, ':', playMin
     dt = list(time.localtime())
     if dt[3] == playHour and dt[4] == playMin:
         soundStart()
